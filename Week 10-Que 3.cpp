c5cf4f0de80d8bd8867bcae9cfc1e6bdc491cd39
Anant gupta <91842337+Anant787@users.noreply.github.com>
2022-06-16 14:06:07 +0530

    Create Week 10-Que 3.cpp

---
+++ Week 10-Que 3.cpp
@@ -0,0 +1,33 @@
+//QUE 30
+//Week 10--Que 3
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Anant Gupta      G       20011921"<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr+0,arr+n);
+    if(((arr[0]==arr[n/2])||(arr[n/2]==arr[n-1]))&&n%2!=0)
+    cout<<"Yes"<<endl;
+    else if(n%2==0&&((arr[0]==arr[n/2])||(arr[n/2]==arr[n-1]&&arr[n/2]==arr[n/2-1])))
+    cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
+    
+    if(n%2!=0)
+    cout<<arr[n/2]<<endl;
+    else{
+        cout<<(arr[(n-1)/2]+arr[n/2])/2.0<<endl;
+    }
+
+    return 0;
+}
